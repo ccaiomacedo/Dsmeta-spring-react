ccd9ebd606b49df2578fd0842c032fbb7f04ee5e
Caio Macedo <caio.macedo-mtm@hotmail.com>
2022-07-11 21:24:19 -0300

    UseState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
